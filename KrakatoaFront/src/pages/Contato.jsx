4d7be854ed9251ffafdd1adb7d425ec9937a87a2
Gabriel <gabriel2001.serafim@gmail.com>
2020-04-30 01:52:54 -0300

    Enviando Arquivo

--- KrakatoaFront/src/pages/Contato.jsx
+++ KrakatoaFront/src/pages/Contato.jsx
@@ -6,6 +6,27 @@ import React, { PureComponent } from 'react';
 import { Container, Grid, Typography } from '@material-ui/core/';
 import Navbar from '../components/Nav';
 import Topo from '../components/Topo';
+import ContatoStyle from '../components/Contato.css';
+const styles = {
+      title: {
+        fontSize:"3.75em",
+        textAlign:"center",
+        margin: "64px",
+        color: "#FF5757",
+      },
+      default :{
+        fontSize:"2.25em",
+        fontWeight: '1000',
+        color: "#FF5757",
+      },
+      name: {
+        backgroundColor: 'gray',
+        color: "#FF5757",
+        width: '30%',
+        height: '50px',
+      }
+}
+
 
 export default class Contato extends PureComponent {
   render() {
@@ -14,6 +35,19 @@ export default class Contato extends PureComponent {
         <Container maxWidth="lg">
           <Topo />
           <Navbar />
+          <section>
+            <Typography style={styles.title}>Contato</Typography>
+          </section>
+          <section>
+          <Typography style={styles.default}>FALE CONOSCO</Typography> 
+          </section>
+          <form>
+              <label>
+                {/* <input className={ContatoStyle} type = "text" name = "name" placeholder = "Nome*" style={styles.name}/>
+                <input className={ContatoStyle} type = "text" name = "name" placeholder = "Nome*" style={styles.name}/>
+                <input className={ContatoStyle} type = "text" name = "name" placeholder = "Nome*" style={styles.name}/> */}
+              </label>
+          </form>
         </Container>
       </>
     );
