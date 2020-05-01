150cd9084b3cef0d72917f6aeefaa51b7fe22ac8
elvisserafim6 <elvisserafim6@gmail.com>
2020-05-01 03:50:08 -0300

    Finalizando página de produtos

--- KrakatoaFront/src/pages/Produtos.jsx
+++ KrakatoaFront/src/pages/Produtos.jsx
@@ -4,7 +4,16 @@ import React, { PureComponent } from 'react';
 import { Container, Grid, Typography } from '@material-ui/core/';
 import Navbar from '../components/Nav';
 import Topo from '../components/Topo';
+import ProductList from '../components/Produtos'
 
+const styles = {
+  title: {
+    fontSize: "3.75em",
+    textAlign: "center",
+    margin: "64px",
+    color: "#FF5757",
+  },
+}
 export default class Produtos extends PureComponent {
   render() {
     return (
@@ -12,6 +21,8 @@ export default class Produtos extends PureComponent {
         <Container maxWidth="lg">
           <Topo />
           <Navbar />
+          <Typography style={styles.title}>Kangas</Typography>
+          <ProductList />
         </Container>
       </>
     );
