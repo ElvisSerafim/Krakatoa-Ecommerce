e44e81a979e3a76f0d2681011e44eb0895406373
Gabriel <gabriel2001.serafim@gmail.com>
2020-05-04 01:10:55 -0300

    Arquivo Enviado

--- KrakatoaFront/src/pages/MinhaConta.jsx
+++ KrakatoaFront/src/pages/MinhaConta.jsx
@@ -1,8 +1,44 @@
 import React, { PureComponent } from 'react';
-import { Container, Grid, Typography } from '@material-ui/core/';
+import { Container, Grid, Typography, Box } from '@material-ui/core/';
+import MinhaConta from '../components/MinhaConta';
 import Navbar from '../components/Nav';
 import Topo from '../components/Topo';
+import Footer from '../components/Footer';
 
+var styles = {
+  title: {
+    fontSize: '2.25em',
+    fontWeight: 'bold',
+    margin: '64px',
+    color: '#FF5757',
+  },
+  fundo: {
+    backgroundColor: '#C8C8C8',
+    AlignItems: 'center',
+    borderRadius: 20
+  },
+  flexGlobal: {
+    display: 'flex',
+    flexDirection: 'column',
+    flex: 1,
+  },
+  topico: {
+    fontSize: '1.25em',
+    fontWeight: 'bold',
+    marginTop: '64px',
+    marginLeft: '64px',
+    color: 'WHITE',
+  },
+  input: {
+    marginLeft: '64px',
+    marginTop: '32px',
+  },
+  flexTopico: {
+    display: 'flex',
+    flexDirection: 'row',
+    flex: 1,
+  },
+};
 export default class Login extends PureComponent {
   render() {
     return (
@@ -10,7 +46,22 @@ export default class Login extends PureComponent {
         <Container maxWidth="lg">
           <Topo />
           <Navbar />
+          <Typography style={styles.title}>MINHA CONTA</Typography>
+          <Box style={styles.fundo}>
+            <div style={styles.flexGlobal}>
+              <div style={styles.flexTopico}>
+              <Typography style={styles.topico}>Entrar</Typography>
+              <div style={{paddingLeft:626}}>
+              <Typography style={styles.topico}>Registrar</Typography>
+              </div>
+              </div>
+              <div style={styles.input}>
+                <MinhaConta />
+              </div>
+            </div>
+          </Box>
         </Container>
+        <Footer />
       </>
     );
   }
