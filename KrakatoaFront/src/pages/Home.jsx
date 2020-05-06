49b3a03a4b0a2091752f4cdb508d6f548db2d5b4
Gabriel <gabriel2001.serafim@gmail.com>
2020-05-06 00:05:18 -0300

    Arquivo Enviado

--- KrakatoaFront/src/pages/Home.jsx
+++ KrakatoaFront/src/pages/Home.jsx
@@ -6,7 +6,8 @@ import Navbar from '../components/Nav';
 import Topo from '../components/Topo';
 import Promos from '../components/promos';
 import ApiService from '../Services/ApiService';
-
+import HomeComp from '../components/Home';
+import Footer from '../components/Footer';
 const styles = {
   Promos: { width: 'auto', height: '373px', backgroundColor: '#B1B1B1' },
   container: {
@@ -24,17 +25,20 @@ const styles = {
 };
 
 export default class Home extends PureComponent {
-
   render() {
     return (
       <>
-        <Container maxWidth="lg">
-          <Topo />
-          <Navbar />
-          <Grid container spacing={2} justify="space-evenly">
+      <Container maxWidth="lg">
+      <Topo />
+      <div style={{ marginBottom: 64 }}>
+            <Navbar />
+          </div>
+      </Container>
             <Grid item lg={12} md={2}>
-              <div style={styles.Carrocel}>1</div>
+              <HomeComp />
             </Grid>
+        <Container maxWidth="lg">
+          <Grid container spacing={2} justify="space-evenly">
             <Grid item lg={12} md={2}>
               <Promos />
             </Grid>
@@ -52,6 +56,7 @@ export default class Home extends PureComponent {
             </Grid>
           </Grid>
         </Container>
+        <Footer/>
       </>
     );
   }
