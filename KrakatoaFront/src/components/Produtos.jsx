3215684df5b1f09cfd153adaea3d24ee1f5231b0
Focadecombate <gustavosisa@outlook.com>
2020-05-09 02:01:46 -0300

    Teste

--- KrakatoaFront/src/components/Produtos.jsx
+++ KrakatoaFront/src/components/Produtos.jsx
@@ -1,31 +1,33 @@
 /* eslint-disable react/prop-types */
-import React, { Component } from 'react';
-import { useSelector, useDispatch} from 'react-redux';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Grid from '@material-ui/core/Grid';
+import { Link } from 'react-router-dom';
 import Produto from './Produto';
-import {addCart} from '../reducers/productsCart'
+import { addCart } from '../reducers/productsCart';
 
-const Produtos = () => {
-
+const Produtos = ({ title }) => {
   const dispatch = useDispatch();
-
-  const addItemCart = (product) => {
-    console.log(product);
-    dispatch(addCart(product));
-  }
-  const products = useSelector((state) => state.products)
+  const addItemCart = (productCart) => {
+    console.log(productCart);
+    dispatch(addCart(productCart));
+  };
+  const lower = title.toLowerCase();
+  const products = useSelector((state) => state.products);
   return (
     products && products.map((item) => (
       <Grid container justify="flex-start" spacing="2">
         {item.map((value) => (
-          <Grid key={value} item lg={3}>
-            <Produto produto={value} addItem={addItemCart}/>
+          <Grid key={value.id} item lg={3}>
+            <Link to={`${lower}/${value.id}`} style={{ textDecoration: 'none' }}>
+              <Produto produto={value} addItem={addItemCart} />
+            </Link>
           </Grid>
         ))}
       </Grid>
     ))
   );
-}
+};
 
 export default Produtos;
 
