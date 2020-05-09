2c72d01434ce59bcb556b70b5d46d36929bc32c8
Gabriel <gabriel2001.serafim@gmail.com>
2020-05-09 02:54:49 -0300

    Arquivo Enviado

---
+++ KrakatoaFront/src/reducers/search.js
@@ -0,0 +1,17 @@
+const INITIAL_STATE = {};
+
+export default function reducer(state = INITIAL_STATE, action) {
+    if(action.type == 'SEARCH'){
+        console.log('SEARCH: ' + action.payload );
+        return action.payload;
+    }
+    return state;
+}
+
+
+export const sendSearch = (payload) => {
+    return {
+        type: 'SEARCH',
+        payload
+    }
+}
