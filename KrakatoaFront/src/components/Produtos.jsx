924b53129debca4d7b08ba92803649058d65c800
elvisserafim6 <elvisserafim6@gmail.com>
2020-07-01 22:22:00 -0300

    Colocando mensagem de sem produtos na categoria

--- KrakatoaFront/src/components/Produtos.jsx
+++ KrakatoaFront/src/components/Produtos.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Grid from '@material-ui/core/Grid';
+import { Typography } from '@material-ui/core/';
 import { makeStyles } from '@material-ui/core/styles';
 import NoSsr from '@material-ui/core/NoSsr';
 import Produto from './Produto';
@@ -10,6 +11,7 @@ import ComboBox from './ComboBox';
 import api from '../Services/ApiService';
 import Hidden from '@material-ui/core/Hidden';
 import { updateProducts } from '../reducers/products';
+import Estilos from '../Estilos';
 
 const useStyles = makeStyles({
   GridContainer: {
@@ -41,34 +43,34 @@ const Produtos = ({ title, alert, name }) => {
     const arrayAux = products[0];
 
     if (value === '') return;
-    if (value === 'Mais vendidos'){
-       arrayAux.sort((a, b) => {
-        if(a.vendas < b.vendas){
-           return -1;
-        }
-        if (a.vendas > b.vendas){
-          return 1;
-        }
-        return 0;
-      });
-    } 
-    if (value === 'Menor Preço'){
+    if (value === 'Mais vendidos') {
       arrayAux.sort((a, b) => {
-        if(a.preco < b.preco){
-           return -1;
+        if (a.vendas < b.vendas) {
+          return -1;
         }
-        if (a.preco > b.preco){
+        if (a.vendas > b.vendas) {
           return 1;
         }
         return 0;
       });
     }
-    if (value === 'Maior Preço'){
+    if (value === 'Menor Preço') {
       arrayAux.sort((a, b) => {
-        if(a.preco > b.preco){
-           return -1;
+        if (a.preco < b.preco) {
+          return -1;
         }
-        if (a.preco < b.preco){
+        if (a.preco > b.preco) {
+          return 1;
+        }
+        return 0;
+      });
+    }
+    if (value === 'Maior Preço') {
+      arrayAux.sort((a, b) => {
+        if (a.preco > b.preco) {
+          return -1;
+        }
+        if (a.preco < b.preco) {
           return 1;
         }
         return 0;
@@ -80,54 +82,66 @@ const Produtos = ({ title, alert, name }) => {
   };
   const [state, setState] = React.useState({ open: false, defer: false });
   return products.map((item) => (
-      <Grid
-        container
-        justify="space-evenly"
-        spacing={2}
-        className={classes.GridContainer}
-      >
-        {name == 'pesquisa' ? (
-          <Grid container lg={12} style={{ flexDirection: 'row-reverse' }}>
-          </Grid>
-        ) : <Grid container lg={12} style={{ flexDirection: 'row-reverse' }}>
-            <ComboBox
-              onChange={(event) => {
-                setOrderBy(event.target.value);
-                ordenar(event.target.value);
-              }}
-              style={{ maxWidth: 300 }}
-              value={orderBy}
-              items={['Mais vendidos', 'Menor Preço', 'Maior Preço']}
-              label="Ordenar por: "
-            />
-          </Grid>}
+    <Grid
+      container
+      justify="space-evenly"
+      spacing={2}
+      className={classes.GridContainer}
+    >
+      {products[0].length == 0  ? (
+        <Grid container lg={12} style={{ flexDirection: 'row-reverse' }}>
+        </Grid>
+      ) : <Grid container lg={12} style={{ flexDirection: 'row-reverse' }}>
+          <ComboBox
+            onChange={(event) => {
+              setOrderBy(event.target.value);
+              ordenar(event.target.value);
+            }}
+            style={{ maxWidth: 300 }}
+            value={orderBy}
+            items={['Mais vendidos', 'Menor Preço', 'Maior Preço']}
+            label="Ordenar por: "
+          />
+        </Grid>}
+
+      {products[0].length == 0 ? (
+        <div style={{ ...Estilos.flexRowCENTER, paddingTop: 64, paddingBottom: 64 }}>
+          <Typography
+            color="primary"
+            variant="h5"
+            style={{ fontSize: '3.0em', fontWeight: 'Bold' }}
+          >
+            Sem produtos na categoria
+            </Typography>
+        </div>
+      ) : (
+          <NoSsr defer>
+            {item.map((value) => (
+              <Grid
+                key={value.id}
+                item
+                lg={3}
+                md={4}
+                sm={6}
+                xs={6}
+                className={classes.product}
+              >
+                <NoSsr defer>
+                  <Produto
+                    produto={value}
+                    update={() => { }}
+                    title={lower}
+                    addItem={addItemCart}
+                  />
+                </NoSsr>
+              </Grid>
+            ))}
+          </NoSsr>
+        )}
+
+    </Grid>
 
 
-        <NoSsr defer>
-          {item.map((value) => (
-            <Grid
-              key={value.id}
-              item
-              lg={3}
-              md={4}
-              sm={6}
-              xs={6}
-              className={classes.product}
-            >
-              <NoSsr defer>
-                <Produto
-                  produto={value}
-                  update={() => { }}
-                  title={lower}
-                  addItem={addItemCart}
-                />
-              </NoSsr>
-            </Grid>
-          ))}
-        </NoSsr>
-      </Grid>
-
-   
   ));
 };
 
