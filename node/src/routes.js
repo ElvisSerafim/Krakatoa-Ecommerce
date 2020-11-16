35c22171e01ee16b082a669332d3b6f8574a4d98
Gustavo Santos <gustavosisa@outlook.com>
2020-11-16 19:18:08 -0300

    Separando o backend

--- node/src/routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const CorreioController = require("./controllers/CorreiosController");
-
-const routes = Router();
-
-routes.post("/api/calcFrete", CorreioController.calcularPrazoPreco);
-
-module.exports = routes;
